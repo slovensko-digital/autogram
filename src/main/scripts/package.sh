0b21090dcdefea940f65fdaf5bf1fe21dbc4118c
Jakub Duras <jakub@duras.me>
2021-05-15 23:16:17 +0200

    Add complete support for windows packaging #1

--- src/main/scripts/package.sh
+++ src/main/scripts/package.sh
@@ -13,28 +13,70 @@ do
 done < "./build.properties"
 unset IFS
 
-# Defaults for optional properties
-# TODO: version, copyright
-
-# TODO: Use platform-specific properties with MSCAPI open on Windows
-
 jpackage=$1
 appDirectory=$2
 jdkDirectory=$3
-platform=$4
-output=$5
-jvmOptions="--illegal-access=warn --add-opens jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED"
+resourcesDir=$4
+platform=$5
+version=$6
+output=$7
 
-$jpackage\
-    --input "$appDirectory"\
-    --runtime-image "$jdkDirectory"\
-    --main-jar ./whitelabel.jar\
-    --name "$properties_name"\
-    --app-version "$properties_version"\
-    --copyright "$properties_copyright"\
-    --vendor "$properties_vendor"\
-    --java-options "$jvmOptions"\
-    --icon ./icon.png\
-    --license-file "$appDirectory/LICENSE"\
-    --dest "$output"
+jvmOptions="--illegal-access=warn --add-opens jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED"
+arguments=(
+    "--input" "$appDirectory"
+    "--runtime-image" "$jdkDirectory"
+    "--main-jar" "whitelabel.jar"
+    "--name" "$properties_name"
+    "--app-version" "${properties_version:-$version}"
+    "--copyright" "$properties_copyright"
+    "--vendor" "$properties_vendor"
+    "--icon" "$resourcesDir/Octosign.png"
+    "--license-file" "$appDirectory/LICENSE"
+    "--resource-dir" "$resourcesDir"
+    "--dest" "$output"
+    "--verbose"
+)
+
+if [[ "$platform" == "win" ]]; then
+    cp "$resourcesDir/main.template.wxs" "$resourcesDir/main.wxs"
+    sed -i -e "s/PROTOCOL_NAME/$properties_protocol/g" "$resourcesDir/main.wxs"
+
+    arguments+=(
+        "--type" "msi"
+        "--icon" "$resourcesDir/Octosign.ico"
+        "--java-options" "$jvmOptions --add-opens jdk.crypto.mscapi/sun.security.mscapi=ALL-UNNAMED"
+    )
+
+    if [[ ! -z "$properties_win_upgradeUUID"  ]]; then
+        arguments+=(
+            "--win-upgrade-uuid" "$properties_win_upgradeUUID"
+        )
+    fi
+
+    if [[ -z "$properties_win_menu" ]] || [[ "$properties_win_menu" -ne "0" ]]; then
+        arguments+=(
+            "--win-menu"
+            "--win-menu-group" "${properties_win_menuGroup:-$properties_vendor}"
+        )
+    fi
+
+    if [[ "$properties_win_perUserInstall" == "1" ]]; then
+        arguments+=(
+            "--win-per-user-install"
+        )
+    fi
+
+    if [[ "$properties_win_shortcut" == "1" ]]; then
+        arguments+=(
+            "--win-shortcut"
+        )
+    fi
+
+fi
+
+# TODO: Use platform-specific Linux properties
+
+# TODO: Use platform-specific macOS properties
+
+$jpackage "${arguments[@]}"
 # TODO: Use --resource-dir for resource overrides
