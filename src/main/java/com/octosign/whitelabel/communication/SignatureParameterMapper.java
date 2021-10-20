227039b1121907a6142828c5e2146d547a102d86
michal-rohacek <michal.rohacek.256@gmail.com>
2021-10-20 16:49:14 +0200

    Cleanup a bit and simplify Mapper

--- src/main/java/com/octosign/whitelabel/communication/SignatureParameterMapper.java
+++ src/main/java/com/octosign/whitelabel/communication/SignatureParameterMapper.java
@@ -1,37 +1,25 @@
 package com.octosign.whitelabel.communication;
 
 import com.google.common.collect.ImmutableMap;
-import com.octosign.whitelabel.ui.Main;
-import eu.europa.esig.dss.AbstractSignatureParameters;
 import eu.europa.esig.dss.asic.xades.ASiCWithXAdESSignatureParameters;
-import eu.europa.esig.dss.enumerations.*;
-import eu.europa.esig.dss.model.InMemoryDocument;
-import eu.europa.esig.dss.pades.PAdESSignatureParameters;
-import eu.europa.esig.dss.pades.SignatureFieldParameters;
-import eu.europa.esig.dss.pades.SignatureImageParameters;
-import eu.europa.esig.dss.pades.SignatureImageTextParameters;
-import eu.europa.esig.dss.pdf.pdfbox.visible.PdfBoxNativeFont;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
-
-import javax.xml.crypto.dsig.CanonicalizationMethod;
-import java.awt.*;
-import java.util.Map;
-import java.util.Objects;
 import eu.europa.esig.dss.enumerations.ASiCContainerType;
 import eu.europa.esig.dss.enumerations.DigestAlgorithm;
 import eu.europa.esig.dss.enumerations.SignatureLevel;
 import eu.europa.esig.dss.enumerations.SignaturePackaging;
+import eu.europa.esig.dss.pades.PAdESSignatureParameters;
 
+import javax.xml.crypto.dsig.CanonicalizationMethod;
+import java.util.Map;
 
-import static com.octosign.whitelabel.communication.SignatureParameters.CanonicalizationMethod.EXCLUSIVE;
 import static com.octosign.whitelabel.communication.SignatureParameters.CanonicalizationMethod.INCLUSIVE;
 import static com.octosign.whitelabel.communication.SignatureParameters.Container.ASICE;
 import static com.octosign.whitelabel.communication.SignatureParameters.Container.ASICS;
 import static com.octosign.whitelabel.communication.SignatureParameters.DigestAlgorithm.*;
 import static com.octosign.whitelabel.communication.SignatureParameters.Format.PADES;
 import static com.octosign.whitelabel.communication.SignatureParameters.Format.XADES;
-import static com.octosign.whitelabel.communication.SignatureParameters.Level.*;
-import static com.octosign.whitelabel.communication.SignatureParameters.Packaging.*;
+import static com.octosign.whitelabel.communication.SignatureParameters.Level.BASELINE_B;
+import static com.octosign.whitelabel.communication.SignatureParameters.Packaging.ENVELOPED;
+import static com.octosign.whitelabel.communication.SignatureParameters.Packaging.ENVELOPING;
 import static eu.europa.esig.dss.enumerations.SignatureLevel.PAdES_BASELINE_B;
 import static eu.europa.esig.dss.enumerations.SignatureLevel.XAdES_BASELINE_B;
 
@@ -39,9 +27,9 @@ public class SignatureParameterMapper {
     private static final Map<SignatureParameters.Level, Map<SignatureParameters.Format, SignatureLevel>> signatureLevelMapping =
         ImmutableMap.of(
             BASELINE_B, ImmutableMap.of(
-                XADES, XAdES_BASELINE_B,
-                PADES, PAdES_BASELINE_B
-            )
+                            XADES, XAdES_BASELINE_B,
+                            PADES, PAdES_BASELINE_B
+                        )
         );
 
     private static final Map<SignatureParameters.Container, ASiCContainerType> asicContainerTypeMapping =
@@ -74,34 +62,22 @@ public class SignatureParameterMapper {
     public static SignaturePackaging map(SignatureParameters.Packaging packaging) { return signaturePackagingMapping.get(packaging); }
     public static String map(SignatureParameters.CanonicalizationMethod canonicalizationMethod) { return canonicalizationMethodMapping.get(canonicalizationMethod); }
 
-    public static AbstractSignatureParameters<?> map(SignatureParameters source) {
-        return switch (source.getFormat()) {
-            case XADES: yield buildXAdESParameters(source);
-            case PADES: yield buildPAdESParameters(source);
-
-            default: throw new AssertionError();
-        };
-    }
-
-    private static ASiCWithXAdESSignatureParameters buildXAdESParameters(SignatureParameters sp) {
+    public static ASiCWithXAdESSignatureParameters mapXAdESParameters(SignatureParameters sp) {
         var parameters = new ASiCWithXAdESSignatureParameters();
 
         parameters.aSiC().setContainerType(map(sp.getContainer()));
-
         parameters.setSignatureLevel(map(sp.getLevel()));
         parameters.setSignaturePackaging(map(sp.getPackaging()));
         parameters.setDigestAlgorithm(map(sp.getDigestAlgorithm()));
         parameters.setSigningCertificateDigestMethod(map(sp.getDigestAlgorithm()));
         parameters.setSignedInfoCanonicalizationMethod(map(sp.getInfoCanonicalization()));
         parameters.setSignedPropertiesCanonicalizationMethod(map(sp.getPropertiesCanonicalization()));
-
-        // parameters.aSiC().setMimeType(sp.getFileMimeType());
         parameters.setEn319132(sp.isEn319132());
 
         return parameters;
     }
 
-    private static PAdESSignatureParameters buildPAdESParameters(SignatureParameters sp) {
+    public static PAdESSignatureParameters mapPAdESParameters(SignatureParameters sp) {
         var parameters = new PAdESSignatureParameters();
 
         parameters.setSignatureLevel(map(sp.getLevel()));
