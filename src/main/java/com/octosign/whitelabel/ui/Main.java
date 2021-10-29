e0bd46b535585b4525c256efa4c8b73e01a74ab4
michal-rohacek <michal.rohacek.256@gmail.com>
2021-10-29 15:24:59 +0200

    Push tiny change

--- src/main/java/com/octosign/whitelabel/ui/Main.java
+++ src/main/java/com/octosign/whitelabel/ui/Main.java
@@ -29,7 +29,8 @@ public class Main extends Application {
     
     public enum Status {
         LOADING,
-        READY
+        READY,
+        ERROR
     }
 
     private static final Locale skLocale = new Locale( "sk");
