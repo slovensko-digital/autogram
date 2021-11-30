7e7985609e03796271ac1530eb3f3398388562c8
michal-rohacek <michal.rohacek.256@gmail.com>
2021-11-30 09:27:38 +0100

    Add entity representing known signing and crypto standards

---
+++ src/main/java/com/octosign/whitelabel/signing/API.java
@@ -0,0 +1,24 @@
+package com.octosign.whitelabel.signing;
+
+import com.octosign.whitelabel.signing.token.MSCAPIToken;
+import com.octosign.whitelabel.signing.token.PKCS11Token;
+import com.octosign.whitelabel.signing.token.PKCS12Token;
+import com.octosign.whitelabel.ui.PasswordCallback;
+
+import java.util.function.Function;
+
+public enum API {
+    PKCS11 ((driver) -> new PKCS11Token(driver.path(), new PasswordCallback())),
+    PKCS12 ((driver) -> new PKCS12Token(driver.path())),
+    MSCAPI ((__) -> new MSCAPIToken());
+
+    private Function<Driver, Token> instantiation;
+
+    API(final Function<Driver, Token> instantiation) {
+        this.instantiation = instantiation;
+    }
+
+    public Token createToken(Driver driver) {
+        return instantiation.apply(driver);
+    }
+}
