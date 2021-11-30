2451c2c940deb0774f65f14a560dc61664ab1510
michal-rohacek <michal.rohacek.256@gmail.com>
2021-11-30 09:31:05 +0100

    Add class representing driver for the signing token

--- src/main/java/com/octosign/whitelabel/signing/Driver.java
+++ src/main/java/com/octosign/whitelabel/signing/Driver.java
@@ -1,45 +1,81 @@
 package com.octosign.whitelabel.signing;
 
-import com.octosign.whitelabel.ui.OS;
-import com.octosign.whitelabel.ui.Selectable;
+import com.octosign.whitelabel.ui.SelectableItem;
 
-import java.io.File;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
-import static com.octosign.whitelabel.ui.Utils.isNullOrBlank;
+import static java.util.Objects.requireNonNull;
 
-public class Driver implements Selectable {
+public class Driver implements SelectableItem {
     private final String name;
-    private final OS os;
-    private final String path;
+    private final API api;
+    private final Map<OperatingSystem, Path> files;
 
-    public Driver(String name, OS os, String path) {
-        this.name = name;
-        this.os = os;
-        this.path = path;
-    }
+    private Driver(String name, API api, Map<OperatingSystem, Path> files) {
+        this.name = requireNonNull(name);
+        this.api = requireNonNull(api);
+        this.files = requireNonNull(files);
+   }
 
-    @Override
     public String getName() {
         return name;
     }
 
-    public String getPath() {
-        return path;
+    @Override
+    public String getSimpleName() {
+        return getName();
     }
 
-    public OS getOs() {
-        return os;
+    public API getAPI() {
+        return api;
+    }
+
+    public Path getPath() {
+        return getPathFor(OperatingSystem.current());
+    }
+
+    public String path() {
+        return getPath() == null ? null : getPath().toString();
+    }
+
+    public Path getPathFor(OperatingSystem os) {
+        return files.get(os);
     }
 
     public boolean isInstalled() {
-        if (isNullOrBlank(path))
-            return false;
-
-        return new File(path).exists();
+        return getPath() != null && getPath().toFile().exists();
     }
 
     public boolean isCompatible() {
-        return os == OS.current();
+        return files.containsKey(OperatingSystem.current());
+    }
+
+    public Token createToken() {
+        return api.createToken(this);
+    }
+
+    public static Driver.Builder of(String name) {
+        return new Driver.Builder(name);
+    }
+
+    public static class Builder {
+        private final String name;
+        private final Map<OperatingSystem, Path> items;
+
+        public Builder(String name) {
+            this.name = name;
+            this.items = new HashMap<>();
+        }
+
+        public Builder file(OperatingSystem os, String path) {
+            items.put(os, Path.of(path));
+            return this;
+        }
+
+        public Driver with(API api) {
+            return new Driver(name, api, items);
+        }
     }
 }
-
