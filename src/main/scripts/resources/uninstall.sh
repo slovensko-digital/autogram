9c053fe5fb056187f4461d14011e54312f3dd9da
pom <>
2021-12-02 00:35:52 +0100

    Generate certificates during postinstall (on macos)
    Proof of concept

---
+++ src/main/scripts/resources/uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## This is only file used for testing on macos - it just removes certificates
+
+TLS_DIR="$HOME/Library/Application Support/Octosign/tls"
+
+# Any of these might work
+# security remove-trusted-cert "$TLS_DIR/octosign-cert.pem"
+# security delete-certificate -c "localhost.arcicode.com" "$HOME/Library/Keychains/login.keychain-db"
+security delete-certificate -Z $(openssl x509 -in "$TLS_DIR/octosign-cert.pem" -outform DER | shasum -a 1 ) "$HOME/Library/Keychains/login.keychain-db"
+
+rm -fr "$TLS_DIR"
