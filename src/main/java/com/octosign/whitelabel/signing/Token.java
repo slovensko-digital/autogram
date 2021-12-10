464f6f9c315738f2645ec8a6ab9160f5c92ab823
michal-rohacek <michal.rohacek.256@gmail.com>
2021-12-10 17:07:11 +0100

    Move drivers initialization to superclass

--- src/main/java/com/octosign/whitelabel/signing/Token.java
+++ src/main/java/com/octosign/whitelabel/signing/Token.java
@@ -6,6 +6,9 @@ import eu.europa.esig.dss.token.SignatureTokenConnection;
 
 import java.util.*;
 
+import static com.octosign.whitelabel.signing.KeystoreType.PKCS11;
+import static com.octosign.whitelabel.signing.OperatingSystem.*;
+import static com.octosign.whitelabel.ui.I18n.translate;
 import static com.octosign.whitelabel.ui.Utils.isNullOrEmpty;
 
 public abstract class Token {
@@ -56,20 +59,35 @@ public abstract class Token {
 
 
     public static List<Driver> getAvailableDrivers() {
-        return Token.getDrivers().stream()
-                    .filter(Driver::isCompatible)
-                    .filter(Driver::isInstalled)
-                    .toList();
+        return getDrivers().stream()
+                           .filter(Driver::isCompatible)
+                           .filter(Driver::isInstalled)
+                           .toList();
     }
 
     public static Collection<Driver> getDrivers() {
-        if (DRIVERS == null)
-            DRIVERS = new ArrayList<>();
-
+        if (DRIVERS == null) loadDrivers();
         return DRIVERS;
     }
 
-    protected static void registerDriver(Driver driver) {
-        getDrivers().add(driver);
+    private static void loadDrivers() {
+        DRIVERS = new ArrayList<>();
+
+        /*
+         * PKCS11 drivers
+         */
+        DRIVERS.addAll(List.of(
+                Driver.name(translate("btn.eID"))
+                      .file(WINDOWS, "C:\\Program Files (x86)\\EAC MW klient\\pkcs11_x64.dll")
+                      .file(LINUX, "/usr/lib/eac_mw_klient/libpkcs11_x64.so")
+                      .file(MAC, "/Applications/Aplikacia_pre_eID.app/Contents/pkcs11/libPkcs11.dylib")
+                      .keystore(PKCS11),
+
+                Driver.name(translate("btn.mandateCert"))
+                      .file(WINDOWS, "C:\\Program Files\\I.CA SecureStore\\ICASecureStorePkcs11.dll")
+                      .file(LINUX, "/usr/lib/pkcs11/libICASecureStorePkcs11.so")
+                      .keystore(PKCS11)
+                )
+        );
     }
 }
--- src/main/java/com/octosign/whitelabel/signing/token/PKCS11Token.java
+++ src/main/java/com/octosign/whitelabel/signing/token/PKCS11Token.java
@@ -1,14 +1,10 @@
 package com.octosign.whitelabel.signing.token;
 
 import com.octosign.whitelabel.error_handling.UserException;
-import com.octosign.whitelabel.signing.Driver;
 import com.octosign.whitelabel.signing.Token;
 import eu.europa.esig.dss.token.PasswordInputCallback;
 import eu.europa.esig.dss.token.Pkcs11SignatureToken;
 
-import static com.octosign.whitelabel.signing.KeystoreType.*;
-import static com.octosign.whitelabel.ui.I18n.*;
-import static com.octosign.whitelabel.signing.OperatingSystem.*;
 
 public class PKCS11Token extends Token {
     public PKCS11Token(String path, PasswordInputCallback passwordInputCallback, int slotId) {
@@ -23,18 +19,5 @@ public class PKCS11Token extends Token {
 
     public PKCS11Token(String path, PasswordInputCallback passwordInputCallback) {
         this(path, passwordInputCallback, -1);
-    }
-
-    static {
-        registerDriver(Driver.name(translate("btn.eID"))
-                             .file(WINDOWS, "C:\\Program Files (x86)\\EAC MW klient\\pkcs11_x64.dll")
-                             .file(LINUX, "/usr/lib/eac_mw_klient/libpkcs11_x64.so")
-                             .file(MAC, "/Applications/Aplikacia_pre_eID.app/Contents/pkcs11/libPkcs11.dylib")
-                             .keystore(PKCS11));
-
-        registerDriver(Driver.name(translate("btn.mandateCert"))
-                             .file(WINDOWS, "C:\\Program Files\\I.CA SecureStore\\ICASecureStorePkcs11.dll")
-                             .file(LINUX, "/usr/lib/pkcs11/libICASecureStorePkcs11.so")
-                             .keystore(PKCS11));
     }
 }
