fba8d0e1fb7a88c5f7e7b44ba928cc6e7a446c93
michal-rohacek <michal.rohacek.256@gmail.com>
2021-12-10 22:56:16 +0100

    Update ConfigurationProperties

--- src/main/java/com/octosign/whitelabel/ui/ConfigurationProperties.java
+++ src/main/java/com/octosign/whitelabel/ui/ConfigurationProperties.java
@@ -1,39 +1,25 @@
 package com.octosign.whitelabel.ui;
 
-import com.octosign.whitelabel.error_handling.Code;
-import com.octosign.whitelabel.error_handling.IntegrationException;
-
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Properties;
-
 
 public class ConfigurationProperties {
     private static final Properties PROPERTIES;
 
-    public static String getProperty(String key) {
-        if (!PROPERTIES.containsKey(key))
-            throw new IntegrationException(Code.UNEXPECTED_ERROR, "Requested unknown property key: " + key);
-
-        return PROPERTIES.getProperty(key);
-    }
-
-    public static String[] getPropertyArray(String key) {
-        assert(key.startsWith("[]"));
-
-        return Arrays.stream(getProperty(key).split("\\w*<@>\\w*"))
-            .map(String::strip)
-            .toArray(String[]::new);
-    }
-
     static {
         PROPERTIES = new Properties();
-
         try {
             PROPERTIES.load(ConfigurationProperties.class.getResourceAsStream("configuration.properties"));
         }
         catch (IOException e) {
-            throw new IntegrationException(Code.UNEXPECTED_ERROR, e);
+            throw new RuntimeException("Unable to load ConfigurationProperties", e);
         }
+    }
+
+    public static String getProperty(String key) {
+        if (!PROPERTIES.containsKey(key))
+            throw new RuntimeException("Requested unknown property key: " + key);
+
+        return PROPERTIES.getProperty(key);
     }
 }
