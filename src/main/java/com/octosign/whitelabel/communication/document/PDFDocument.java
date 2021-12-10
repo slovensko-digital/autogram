01bb822b878923af148554272b56c02dbe1b3f54
michal-rohacek <michal.rohacek.256@gmail.com>
2021-12-10 16:09:25 +0100

    Delete static MimeTypes in document subclasses

--- src/main/java/com/octosign/whitelabel/communication/document/PDFDocument.java
+++ src/main/java/com/octosign/whitelabel/communication/document/PDFDocument.java
@@ -1,14 +1,9 @@
 package com.octosign.whitelabel.communication.document;
 
-import com.octosign.whitelabel.communication.MimeType;
-
 /**
  * PDF document for signing
  */
 public class PDFDocument extends Document {
-
-    public static final MimeType mimeType = MimeType.PDF;
-
     public PDFDocument() { }
 
     public PDFDocument(Document document) {
