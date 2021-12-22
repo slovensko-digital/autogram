01e37291cb65525d3aa0a6b31907dd4ed174ad8d
michal-rohacek <michal.rohacek.256@gmail.com>
2021-12-22 19:09:39 +0100

    Refactor PKCS12 constructors

--- src/main/java/com/octosign/whitelabel/signing/token/PKCS12Token.java
+++ src/main/java/com/octosign/whitelabel/signing/token/PKCS12Token.java
@@ -10,16 +10,15 @@ import java.security.KeyStore;
 public class PKCS12Token extends Token {
 
     public PKCS12Token(String path, PasswordInputCallback passwordInputCallback) {
-        Pkcs12SignatureToken token = null;
-
         // Find out if password is required with empty password
         // This way user isn't prompted for password if not required
         try {
             var passwordProtection = new KeyStore.PasswordProtection("".toCharArray());
-            token = new Pkcs12SignatureToken(path, passwordProtection);
+            var token = new Pkcs12SignatureToken(path, passwordProtection);
             initialize(token);
         } catch (Exception e) {
-            if (e.getMessage().contains("password was incorrect")) {
+            var cause = e.getCause();
+            if (cause != null && cause.getMessage() != null && cause.getMessage().contains("password was incorrect")) {
                 createToken(path, passwordInputCallback.getPassword());
             } else {
                 throw new UserException("error.tokenAccessDenied.header", "error.tokenAccessDenied.description", e);
