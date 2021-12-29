f2121ccf2ac7ebb6de1502b424b48e51dfdfc254
michal-rohacek <michal.rohacek.256@gmail.com>
2021-12-29 08:36:19 +0100

    Add Mac driver path

--- src/main/java/com/octosign/whitelabel/signing/Token.java
+++ src/main/java/com/octosign/whitelabel/signing/Token.java
@@ -85,6 +85,7 @@ public abstract class Token {
                       .tokenType(translate("text.mandateCertificate"))
                       .file(WINDOWS, "C:\\Program Files\\I.CA SecureStore\\ICASecureStorePkcs11.dll")
                       .file(LINUX, "/usr/lib/pkcs11/libICASecureStorePkcs11.so")
+                      .file(MAC, "/usr/local/lib/pkcs11/libICASecureStorePkcs11.dylib")
                       .keystore(PKCS11)
                 )
         );
