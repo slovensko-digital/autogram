34d7ecf90d73883948ed860acb0317f1fd187a16
michal-rohacek <michal.rohacek.256@gmail.com>
2022-01-10 20:57:40 +0100

    Get rid of GITHUB_TOKEN env variable

--- src/main/java/com/octosign/whitelabel/ui/UpdateNotifier.java
+++ src/main/java/com/octosign/whitelabel/ui/UpdateNotifier.java
@@ -1,6 +1,7 @@
 package com.octosign.whitelabel.ui;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import java.io.IOException;
@@ -9,31 +10,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.Map;
 
 import static com.octosign.whitelabel.ui.FXUtils.displayInfo;
 import static com.octosign.whitelabel.ui.I18n.translate;
 
 public class UpdateNotifier {
-    private static final Map<String, String> ENV = System.getenv();
-
-    private static final String TOKEN = ENV.get("GITHUB_TOKEN");
-    private static final String RELEASES_URL = ENV.get("GITHUB_API_URL") + "/repos/" + ENV.get("GITHUB_REPOSITORY") + "/releases";
-
-    private static String currentVersion() {
-        return Main.getVersion();
-    }
+    private static final String RELEASES_URL = env("GITHUB_API_URL") + "/repos/" + env("GITHUB_REPOSITORY") + "/releases";
 
     public static void checkForUpdates() {
+        //Update disabled in DEV environment
         if (currentVersion().equals("dev")) {
-            System.out.println("Update check disabled in DEV environment.");
             return;
         }
 
-        HttpRequest request = null;
+        HttpRequest request;
         try {
             request = HttpRequest.newBuilder().uri(new URI(RELEASES_URL + "/latest"))
-                                 .header("Authorization", "token " + TOKEN)
                                  .header("Accept", "application/vnd.github.v3+json")
                                  .GET().build();
         } catch (URISyntaxException e) {
@@ -42,20 +34,29 @@ public class UpdateNotifier {
 
         HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(10)).build();
 
-        HttpResponse<String> response = null;
+        HttpResponse<String> response;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (IOException | InterruptedException ignored) {
-            // TODO just log, no app flow interruption needed as this should be only a one-timer
             return;
         }
 
-        JsonObject responseBody = new Gson().fromJson(response.body(), JsonObject.class);
-        String latestVersion = responseBody.get("tag_name").getAsString();
+        JsonElement tagName = new Gson().fromJson(response.body(), JsonObject.class).get("tag_name");
+
+        if (tagName == null) return;
+         String latestVersion = tagName.getAsString();
 
         if (!latestVersion.equalsIgnoreCase(currentVersion())) {
             displayInfo("info.updateAvailable.header", translate("info.updateAvailable.description", latestVersion, currentVersion(), RELEASES_URL));
         }
+    }
+
+    private static String env(String key) {
+        return System.getenv().get(key);
+    }
+
+    private static String currentVersion() {
+        return Main.getVersion();
     }
 
 }
