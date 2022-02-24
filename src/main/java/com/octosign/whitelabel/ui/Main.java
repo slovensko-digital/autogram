cd084bc4eb10d8ebf27f3aed2ab2e4925b93498b
luciajanikova <19lucia99@gmail.com>
2022-02-24 16:00:25 +0100

    Start app minimized

--- src/main/java/com/octosign/whitelabel/ui/Main.java
+++ src/main/java/com/octosign/whitelabel/ui/Main.java
@@ -111,7 +111,6 @@ public class Main extends Application {
     }
 
     private void exit() {
-        if (statusIndication != null) statusIndication.dispose();
         if (server != null) server.stop();
         Platform.exit();
     }
