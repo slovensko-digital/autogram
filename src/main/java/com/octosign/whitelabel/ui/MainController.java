971738abad78b1b9bb1e64b3ef5ef36524c0ddd3
michal-rohacek <michal.rohacek.256@gmail.com>
2022-03-08 13:33:09 +0100

    Add large bunch of Controller CAdES and user alerts integration code

--- src/main/java/com/octosign/whitelabel/ui/MainController.java
+++ src/main/java/com/octosign/whitelabel/ui/MainController.java
@@ -13,6 +13,7 @@ import javafx.application.Platform;
 import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.web.WebView;
@@ -21,6 +22,7 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.function.*;
 
 import static com.google.common.io.Files.getFileExtension;
@@ -29,6 +31,7 @@ import static com.octosign.whitelabel.signing.token.Token.*;
 import static com.octosign.whitelabel.ui.I18n.*;
 import static com.octosign.whitelabel.ui.utils.FXUtils.*;
 import static com.octosign.whitelabel.ui.utils.Utils.*;
+import static javafx.scene.control.ButtonBar.ButtonData.*;
 
 public class MainController {
     public static final Set<String> ALLOWED_TYPES = new HashSet<>(List.of("pdf", "doc", "docx", "odt", "txt", "xml", "rtf", "png", "gif", "tif", "tiff", "bmp","jpg", "jpeg", "xml", "pdf", "xsd", "xls", "xlsx"));
@@ -40,13 +43,16 @@ public class MainController {
     public Button certSettingsButton;
 
     @FXML
-    public Button showNativeVisualizationButton;
+    public Button nativeVisualizationButton;
 
     @FXML
     private WebView webView;
 
     @FXML
-    private TextArea textArea;
+    private TextArea plaintextView;
+
+    @FXML
+    private Node nativeViewPrompt;
 
     /**
      * Bottom-right button used to load/pick certificate and sign
@@ -64,7 +70,9 @@ public class MainController {
      */
     private SignatureUnit signatureUnit;
 
-    private File cachedFile;
+    private volatile File savedDocument;
+
+    private boolean documentWasVisualized;
 
     /**
      * Consumer of the signed document content on success
@@ -72,6 +80,7 @@ public class MainController {
     private Consumer<byte[]> onSigned;
 
     public void initialize() {
+        mainButton.setText(getProperMainButtonText());
         webView.setContextMenuEnabled(false);
         webView.getEngine().setJavaScriptEnabled(false);
     }
@@ -89,6 +98,8 @@ public class MainController {
     }
 
     public void loadDocument() {
+        mainButton.setText(getProperMainButtonText());
+
         var document = signatureUnit.getDocument();
         var params = signatureUnit.getSignatureParameters();
         var mimeType = signatureUnit.getMimeType();
@@ -99,13 +110,14 @@ public class MainController {
         if (isVisualizationSupported(document, mimeType)) {
             displayVisualization(document, params, mimeType);
         } else {
-            displayNotSupportedType(document);
+            exitOnUnsupportedFormat();
         }
+
+        Platform.runLater(() -> bringToForeground(getCurrentStage(mainButton)));
     }
 
     private boolean isVisualizationSupported(Document document, MimeType mimeType) {
         String filename = document.getFilename();
-
         var extension = isPresent(filename) ? getFileExtension(filename) : mimeType.subType();
 
         return ALLOWED_TYPES.contains(extension);
@@ -113,65 +125,62 @@ public class MainController {
 
     private void displayVisualization(Document document, SignatureParameters params, MimeType mimeType) {
         if (mimeType.is(XML)) {
-            displayXMLVisualization((XMLDocument)document, params);
+            displayXMLVisualization((XMLDocument) document, params);
 
         } else if (mimeType.is(PDF)) {
             displayPDFVisualisation(document);
 
         } else {
-            displayBinaryFileVisualisation(document);
+            displayBinaryFileVisualisation();
         }
     }
 
     private void displayXMLVisualization(XMLDocument document, SignatureParameters params) {
-        String transformation = params.getTransformation();
+        show(webView);
+
         MimeType transformationOutputMimeType = params.getTransformationOutputMimeType();
+        String transformationOutput = document.getTransformed(params.getTransformation());
 
-        String transformationOutput = document.getTransformed(transformation);
+        if (transformationOutputMimeType.is(PLAIN)) {
+            displayPlainTextVisualisation(transformationOutput);
 
-        if (transformationOutputMimeType.is(PLAIN))
-            displayPlainTextVisualisation(transformationOutput);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-        else
+        } else {
             displayHTMLVisualisation(transformationOutput);
-
+        }
     }
 
-    private void displayNotSupportedType(Document document) {
-        hide(webView);
-        mainButton.setDisable(true);
-        certSettingsButton.setDisable(true);
-        textArea.setText(translate("text.visualizationNotSupported", document.getFilename()));
+    private void exitOnUnsupportedFormat() {
+        String prettyTypes = String.join(", ", ALLOWED_TYPES);
+
+        throw new IntegrationException(Code.UNSUPPORTED_FORMAT, translate("error.typeNotSupported.description", prettyTypes));
     }
 
     private void displayPlainTextVisualisation(String visualisation) {
+        show(plaintextView);
         hide(webView);
-        textArea.setText(visualisation);
+        plaintextView.setText(visualisation);
     }
 
-    private void displayBinaryFileVisualisation(Document document) {
+    private void displayBinaryFileVisualisation() {
+        show(nativeViewPrompt);
         hide(webView);
-        show(showNativeVisualizationButton);
-
-        textArea.setText(translate("text.openBinaryFile"));
-        showNativeVisualizationButton.setText(translate("btn.openFile", document.getFilename()));
+        saveToFile(signatureUnit.getDocument());
     }
 
     private void displayHTMLVisualisation(String visualisation) {
-        hide(textArea);
+        show(webView);
 
         var engine = webView.getEngine();
         engine.getLoadWorker().stateProperty().addListener((observable, oldState, newState) -> {
             if (newState == Worker.State.SUCCEEDED) {
-                engine.getDocument()
-                      .getElementById("frame")
-                      .setAttribute("srcdoc", visualisation);
+                engine.getDocument().getElementById("frame").setAttribute("srcdoc", visualisation);
             }
         });
         engine.load(Main.class.getResource("visualization.html").toExternalForm());
     }
 
     private void displayPDFVisualisation(Document document) {
-        hide(textArea);
+        show(webView);
 
         Platform.runLater(() -> {
             var engine = webView.getEngine();
@@ -202,21 +211,17 @@ public class MainController {
     }
 
     private void loadSigners() {
-        hide(showNativeVisualizationButton);
         disableMainButton(translate("btn.loading"));
 
         try {
             var driver = displaySelectDialogIfMany(getAvailableDrivers());
-
             var certificates = Token.fromDriver(driver).getCertificates();
             var signingCertificate = displaySelectDialogIfMany(certificates);
-
             signingManager.setActiveCertificate(signingCertificate);
         } catch (UserException e) {
             displayError(e);
-        } finally {
-            enableMainButton(getProperMainButtonText());
         }
+
         enableMainButton(getProperMainButtonText());
     }
 
@@ -235,31 +240,181 @@ public class MainController {
 
     private void signDocument() {
         disableMainButton(translate("btn.signing"));
+        boolean smoothProcess = true;
 
-        if (fileExists(cachedFile))
-            detectFileModifications();
+        if (signatureUnit.isBinary()) {
+            if (not(documentWasVisualized)) {
+                smoothProcess = false;
+                skipVisualizationDialog().showAndWait().ifPresent(response -> {
+                    if (response.getButtonData() == NO) {
+                        requireVisualization();
+                    }
+                });
+            }
+
+            boolean documentWasModified = fileExists(savedDocument) && checkDocumentModifications();
+
+                if (documentWasModified) {
+                    syncAllWithLatest();
+                    displayWarning("warn.contentModified.header", "warn.contentModified.description");
+                } else if (not(smoothProcess) && documentWasVisualized) {
+                    displayInfo("info.previewSuccess.header", "info.previewSuccess.description");
+                }
+        }
 
         try {
             var signedContent = signingManager.sign(signatureUnit);
             onSigned.accept(signedContent);
-            clearCache();
 
         } catch (UserException e) {
             displayError(e);
         }
 
         enableMainButton(getProperMainButtonText());
+        clearDocumentCache();
     }
 
-    private void detectFileModifications() {
-        byte[] originalContent = signatureUnit.getDocument().getContent();
-        byte[] contentFromDisk = readBytes(cachedFile);
+    private void requireVisualization() {
+        FutureTask<Boolean> task = new FutureTask<Boolean>(() -> {
+        if (performRuntimeVisualization())
+            return true;
 
-        // consider making classes records (immutable) to prevent such sneaky assignments
-        if (not(Arrays.equals(contentFromDisk, originalContent))) {
-            signatureUnit.getDocument().setContent(contentFromDisk);
-            displayWarning("warn.dataToSignChanged.header", "warn.dataToSignChanged.description");
+        if (performDesktopVisualization())
+            return true;
+
+        if (not(documentWasVisualized)) {
+            exitOnUnsupportedFormat();
         }
+        return false;
+        });
+
+        task.run();
+        try {
+            task.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Alert skipVisualizationDialog() {
+        Alert alert = buildWarning("warn.skipVisualization.header", "warn.skipVisualization.description");
+        ButtonType yesType = new ButtonType(translate("btn.skipVisualization.yes"), ButtonBar.ButtonData.YES);
+        ButtonType noType = new ButtonType(translate("btn.skipVisualization.no"), ButtonBar.ButtonData.NO);
+        alert.getButtonTypes().setAll(yesType, noType);
+
+        Button noButton = (Button) alert.getDialogPane().lookupButton(noType);
+//        alert.setOnCloseRequest(Event::consume);
+        return alert;
+    }
+
+    private boolean performRuntimeVisualization() {
+        saveToFile(signatureUnit.getDocument());
+        boolean wasSuccessful = false;
+
+        for (var command: getPlatformSpecificCommands()) {
+            var process = executeNative(command);
+
+            if (process != null) {
+                try {
+                    wasSuccessful = process.onExit().thenApply(p -> p.exitValue() == 0).get();
+                } catch (InterruptedException | ExecutionException ignored) {}
+
+            } else {
+                continue;
+            }
+
+            if (wasSuccessful) break;
+        }
+
+        return wasSuccessful;
+    }
+
+    private String[][] getPlatformSpecificCommands() {
+        String path = savedDocument.getAbsolutePath();
+
+        return switch (OperatingSystem.current()) {
+            case WINDOWS -> new String[][] {
+                            {"cmd.exe", "start", path},
+                            {"cmd.exe", "/c", path},
+                            {"cmd.exe", "\"" + path + "\""},
+                            {"rundll32", "url.dll,FileProtocolHandler" + path},
+                            {"rundll32", "SHELL32.DLL,ShellExec_RunDLL \"" + path + "\""},
+                    };
+            case MAC -> new String[][] {{"/usr/bin/open", path}};
+            case LINUX -> new String[][] {{"xdg-open", path}};
+        };
+    }
+
+    private Process executeNative(String... commands) {
+        try {
+            return new ProcessBuilder(commands).inheritIO().start();
+        } catch (IOException e) {
+            // continue
+        }
+
+        try {
+            return Runtime.getRuntime().exec(commands);
+        } catch (IOException ex) {
+            // continue
+        }
+
+        return null;
+    }
+
+
+    private boolean performDesktopVisualization() {
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().open(savedDocument);
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return false;
+    }
+
+    public void saveToFile(Document document) {
+        try {
+            if (not(fileExists(savedDocument))) {
+                var prefix = Files.getNameWithoutExtension(document.getFilename());
+                var suffix = "." + Files.getFileExtension(document.getFilename());
+
+                savedDocument = File.createTempFile(prefix, suffix);
+//                cachedFile.setExecutable(true, false);
+                Files.write(document.getContent(), savedDocument);
+            }
+            savedDocument.deleteOnExit();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean checkDocumentModifications() {
+        byte[] originalContent = signatureUnit.getDocument().getContent();
+        byte[] contentFromDisk = readBytes(savedDocument);
+
+        return not(Arrays.equals(contentFromDisk, originalContent));
+    }
+
+    private void syncAllWithLatest() {
+        // consider making classes records (immutable) to prevent such sneaky assignments
+        byte[] modifiedContent = readBytes(savedDocument);
+        signatureUnit.getDocument().setContent(modifiedContent);
+    }
+
+    private void clearDocumentCache() {
+        if (fileExists(savedDocument)) {
+            if (not(savedDocument.delete())) {
+                displayWarning("warn.fileNotDeleted.header", "warn.fileNotDeleted.description");
+            }
+        }
+        savedDocument = null;
+        documentWasVisualized = false;
     }
 
     private void disableMainButton(String text) {
@@ -295,6 +450,12 @@ public class MainController {
     }
 
     @FXML
+    private void onNativeVisualizationButtonAction() {
+        requireVisualization();
+        documentWasVisualized = true;
+    }
+
+    @FXML
     private void onAboutButtonAction() {
         new AboutDialog().show();
     }
@@ -302,44 +463,5 @@ public class MainController {
     @FXML
     private void onCertSettingsButtonAction() {
         loadSigners();
-    }
-
-    @FXML
-    private void onShowNativeVisualizationButtonAction() {
-        cacheAsFile(signatureUnit.getDocument());
-
-        new Thread(() -> {
-            try {
-                Desktop.getDesktop().open(cachedFile);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
-
-    public void cacheAsFile(Document document) {
-        try {
-            if (not(fileExists(cachedFile))) {
-                var prefix = Files.getNameWithoutExtension(document.getFilename());
-                var suffix = "." + Files.getFileExtension(document.getFilename());
-
-                cachedFile = File.createTempFile(prefix, suffix);
-                Files.write(document.getContent(), cachedFile);
-            }
-            cachedFile.deleteOnExit();
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void clearCache() {
-        if (fileExists(cachedFile)) {
-            if (not(cachedFile.delete())) {
-                displayWarning("warn.fileNotDeleted.header", "warn.fileNotDeleted.description");
-            }
-
-            cachedFile = null;
-        }
     }
 }
