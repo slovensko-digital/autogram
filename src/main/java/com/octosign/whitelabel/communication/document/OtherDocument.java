5a32593deaa3ff1fa97ded184e4e2a32d8bc2bb6
michal-rohacek <michal.rohacek.256@gmail.com>
2022-03-16 21:04:01 +0100

    Add OtherDocument

---
+++ src/main/java/com/octosign/whitelabel/communication/document/OtherDocument.java
@@ -0,0 +1,12 @@
+package com.octosign.whitelabel.communication.document;
+
+/**
+ * PDF document for signing
+ */
+public class OtherDocument extends Document {
+    public OtherDocument() { }
+
+    public OtherDocument(Document document) {
+        super(document);
+    }
+}
