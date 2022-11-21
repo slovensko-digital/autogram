41c2c1097be879dd28585934f5f5e85a0676245c
jsuchal <jan.suchal@gmail.com>
2022-11-21 12:36:34 +0100

    Fix signing, refs #74

---
+++ src/test/java/com/octosign/whitelabel/communication/document/XMLDocumentTest.java
@@ -0,0 +1,20 @@
+package com.octosign.whitelabel.communication.document;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class XMLDocumentTest {
+
+    @Test
+    void testTransformsXMLFormToXdc() {
+//        doc = new XMLDocument(...);
+//        doc.getTransformed(transformation);
+        assertTrue(false);
+    }
+
+    @Test
+    void testTransformsPlainHtmlInXMLFormToXdc() {
+        assertTrue(false);
+    }
+}
