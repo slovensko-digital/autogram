2748e6147a2498311488b513afe1fd4ffd30a85c
jsuchal <jan.suchal@gmail.com>
2022-11-30 22:53:15 +0100

    Fix empty namespaces in datacontainer & fix signing xml with plain html elements

--- src/test/java/com/octosign/whitelabel/communication/document/XMLDocumentTest.java
+++ src/test/java/com/octosign/whitelabel/communication/document/XMLDocumentTest.java
@@ -1,20 +1,21 @@
 package com.octosign.whitelabel.communication.document;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class XMLDocumentTest {
-
     @Test
-    void testTransformsXMLFormToXdc() {
-//        doc = new XMLDocument(...);
-//        doc.getTransformed(transformation);
-        assertTrue(false);
-    }
+    void testTransformsPlainHtmlWithoutAddingNamespaces() throws IOException {
+        var input = this.getClass().getResourceAsStream("abc.xml").readAllBytes();
+        var transformation = new String(this.getClass().getResourceAsStream("abc.xslt").readAllBytes());
 
-    @Test
-    void testTransformsPlainHtmlInXMLFormToXdc() {
-        assertTrue(false);
+        XMLDocument doc = new XMLDocument(new Document("id1", "test.xml", input));
+        String transformed = doc.getTransformed(transformation);
+
+        assertEquals(-1, transformed.lastIndexOf(":p>"));
     }
 }
