0fa260c9633f5f7c1054ebffb148c809ce8b2a58
Jan Suchal <jan.suchal@slovensko.digital>
2023-04-06 12:32:12 +0200

    Better wording

--- src/main/java/digital/slovensko/autogram/core/errors/PINIncorrectException.java
+++ src/main/java/digital/slovensko/autogram/core/errors/PINIncorrectException.java
@@ -2,6 +2,6 @@ package digital.slovensko.autogram.core.errors;
 
 public class PINIncorrectException extends AutogramException {
     public PINIncorrectException() {
-        super("Nastala chyba", "Zadali ste nesprávny PIN", "Pravdepodobne ste spravili chybu pri zadávaní PINu.\n\nUistite sa, že zadávate správny PIN a skúste znova.");
+        super("Nastala chyba", "Zadali ste nesprávny bezpečnostný kód", "Pravdepodobne ste spravili chybu pri zadávaní bezpečnostného kódu.\n\nUistite sa, že zadávate správny bezpečnostný kód a skúste znova.");
     }
 }
