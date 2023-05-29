eae082784f520470c411c791cb94003f509aed71
pom <stanislavparnicky@gmail.com>
2023-05-29 14:06:47 +0200

    Apple code signing and notarization
    - add code signing step for `pkg` build
    - add required process around code signing to github workflow
    - add notarization step to github workflow
    - move `APPLE_KEYCHAIN_PATH` and  `APPLE_DEVELOPER_IDENTITY` properties from `build.properties` to environment variables so they can be set on CI
    - changes macos installer logo - closes https://github.com/slovensko-digital/autogram/issues/189
    
    refs AG-28

--- src/main/scripts/resources/Autogram-post-image.sh
+++ src/main/scripts/resources/Autogram-post-image.sh
@@ -1,6 +1,5 @@
-TARGET="`cd ../images/*/*/Contents;pwd`"
-SOURCE="`cd ../../mac-launcher;pwd`"
-
+TARGET="$(cd ../images/*/*/Contents;pwd)"
+SOURCE="$(cd ../../mac-launcher;pwd)"
 
 mv "$TARGET/MacOS/Autogram" "$TARGET/MacOS/AutogramApp"
 mv "$TARGET/app/Autogram.cfg" "$TARGET/app/AutogramApp.cfg"
@@ -9,3 +8,7 @@ cp -r "$SOURCE/Resources" "$TARGET"
 cp -r "$SOURCE/MacOS" "$TARGET"
 
 chmod +x "$TARGET/MacOS/Autogram"
+
+# codesign changed executables
+codesign -s "$APPLE_DEVELOPER_IDENTITY" --keychain $APPLE_KEYCHAIN_PATH --options=runtime --deep --timestamp --force "$TARGET/MacOS/Autogram"
+codesign -s "$APPLE_DEVELOPER_IDENTITY" --keychain $APPLE_KEYCHAIN_PATH --options=runtime --deep --timestamp --force "$TARGET/MacOS/AutogramApp"
