58c0b13d5edf500d567b82a1d7b3247d9d5a7aa6
Jan Suchal <jan.suchal@slovensko.digital>
2023-06-14 21:42:56 +0200

    Optimize import

--- src/main/java/digital/slovensko/autogram/server/AutogramServer.java
+++ src/main/java/digital/slovensko/autogram/server/AutogramServer.java
@@ -5,7 +5,6 @@ import com.sun.net.httpserver.HttpsConfigurator;
 import com.sun.net.httpserver.HttpsParameters;
 import com.sun.net.httpserver.HttpsServer;
 import digital.slovensko.autogram.core.Autogram;
-import digital.slovensko.autogram.ui.gui.GUI;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
