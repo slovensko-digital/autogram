c97abfb4e7209f47ebe19766721d92a977e2dcad
Jan Suchal <jan.suchal@slovensko.digital>
2023-06-22 13:30:34 +0200

    Tune wording a bit

--- src/main/java/digital/slovensko/autogram/core/FailedVisualizationException.java
+++ src/main/java/digital/slovensko/autogram/core/FailedVisualizationException.java
@@ -6,9 +6,9 @@ public class FailedVisualizationException extends IgnorableException {
 
     public FailedVisualizationException(Throwable e, SigningJob job, Runnable onContinueCallback) {
         super(
-            "Pri zobrazovaní dokumentu nastala chyba.",
-            "Chcete pokračovať v podpisovaní?", 
-                "Stále je možné dokument podpísať, ale uistite sa, že dôverujete zdroju dokumentu.\nKontaktujte správcu systému a nahláste mu chybu.",
-                e, job, onContinueCallback);
+            "Pri zobrazovaní dokumentu nastala chyba",
+            "Chcete pokračovať v podpisovaní?",
+            "Pri zobrazovaní dokumentu nastala neočakávaná chyba. Dokument je možné podpísať, ale uistite sa, že dôverujete zdroju dokumentu.\n\nKontaktujte správcu systému a nahláste mu chybu.",
+            e, job, onContinueCallback);
     }
 }
