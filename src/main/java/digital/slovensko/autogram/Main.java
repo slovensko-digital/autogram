2918696f01cdf71b1e586678cc0bdb69d1aa0567
Martin <martinb@solver.sk>
2023-06-23 11:45:08 +0200

    Remove starting with args message

--- src/main/java/digital/slovensko/autogram/Main.java
+++ src/main/java/digital/slovensko/autogram/Main.java
@@ -8,8 +8,6 @@ import static java.util.Objects.requireNonNullElse;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starting with args: " + Arrays.toString(args));
-
         AppStarter.start(args);
     }
 
