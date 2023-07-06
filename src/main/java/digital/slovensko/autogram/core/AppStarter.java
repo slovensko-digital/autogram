c7ce20f59e2baf82192c3e9d1c45366f1a93ecda
Marek Ceľuch <celuchmarek@gmail.com>
2023-07-06 10:34:11 +0200

    update help command

--- src/main/java/digital/slovensko/autogram/core/AppStarter.java
+++ src/main/java/digital/slovensko/autogram/core/AppStarter.java
@@ -10,16 +10,16 @@ import java.io.PrintWriter;
 public class AppStarter {
     private static final Options options = new Options().
         addOptionGroup(new OptionGroup().
-            addOption(new Option(null, "url", true, "Start in GUI mode, with HTTP API server listening based on parameters. Application starts minimised when is not empty.")).
+            addOption(new Option(null, "url", true, "Start in GUI mode with API server listening on given port and protocol (HTTP/HTTPS). Application starts minimised when is not empty.")).
             addOption(new Option("c", "cli", false, "Run application in CLI mode."))
         ).
         addOption("h", "help", false, "Print this command line help.").
-        addOption("u", "usage", false, "Print usage tips.").
+        addOption("u", "usage", false, "Print usage examples.").
         addOption("s", "source", true, "Source file or directory of files to sign.").
-        addOption("t", "target", true, "Target file or directory for signed files.").
+        addOption("t", "target", true, "Target file or directory for signed files. Type (file/directory) must match the source.").
         addOption("f", "force", false, "Overwrite existing file(s).").
         addOption(null, "pdfa", false, "Check PDF/A compliance before signing.").
-        addOption(null, "parents", false, "Create all target's parent directories if needed.").
+        addOption(null, "parents", false, "Create all parent directories for target if needed.").
         addOption("d", "driver", true, "PCKS driver for signing. Supported drivers: eid, secure_store, monet, gemalto.");
 
     public static void start(String[] args) {
@@ -43,8 +43,19 @@ public class AppStarter {
 
     public static void printHelp() {
         final HelpFormatter formatter = new HelpFormatter();
-        final String syntax = "autogram [options]";
-        formatter.printHelp(syntax, options);
+        final String syntax = "autogram";
+        final String footer = """
+
+                In CLI mode, signed files are saved with the same name as the source file, but with the suffix "_signed" if no target is specified. If the source is a directory, the target must also be a directory. If the source is a file, the target must also be a file. If the source is a driectory and no target is specified, a target directory is created with the same name as the source directory, but with the suffix "_signed".
+
+                If no target is specified and generated target name already exists, number is added to the target's name suffix if --force is not enabled. For example, if the source is "file.pdf" and the target is not specified, the target will be "file_signed.pdf". If the target already exists, the target will be "file_signed (1).pdf". If that target already exists, the target will be "file_signed (2).pdf", and so on.
+
+                If --force is enabled, the target will be overwritten if it already exists.
+
+                If target is specified with missing parent directories, they are created onyl if --parents is enabled. Otherwise, the signing fails. For example, if the source is "file.pdf" and the target is "target/file_signed.pdf", the target directory "target" must exist. If it does not exist, the signing fails. If --parents is enabled, the target directory "target" is created if it does not exist.
+                """;
+
+        formatter.printHelp(80, syntax, "", options, footer, true);
     }
 
     public static void printUsage() {
