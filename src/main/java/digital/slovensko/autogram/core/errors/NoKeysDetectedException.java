02cc0ddd9b970de73c9127a571f3af430b5f1c8a
Marek Ceľuch <celuchmarek@gmail.com>
2023-10-12 13:19:36 +0200

    reword noKeys and noValidKeys warnings

--- src/main/java/digital/slovensko/autogram/core/errors/NoKeysDetectedException.java
+++ src/main/java/digital/slovensko/autogram/core/errors/NoKeysDetectedException.java
@@ -2,6 +2,6 @@ package digital.slovensko.autogram.core.errors;
 
 public class NoKeysDetectedException extends AutogramException {
     public NoKeysDetectedException() {
-        super("Nastala chyba", "Nenašli sa žiadne podpisové klúče", "Na karte sa pravdepodobne nenachádzajú žiadne klúče, ktoré by sa dali použiť na podpisovanie.\n\nV prípade nového občianskeho preukazu to môže znamenať, že si potrebujete certifikáty na podpisovanie cez občiansky preukaz vydať. Robí sa to pomocou eID klienta.", null);
+        super("Nastala chyba", "Nenašli sa žiadne podpisové certifikáty", "V úložisku certifikátov sa pravdepodobne nenachádzajú žiadne platné podpisové certifikáty, ktoré by sa dali použiť na podpisovanie.\n\nV prípade nového občianskeho preukazu to môže znamenať, že si potrebujete certifikáty na podpisovanie cez občiansky preukaz vydať. Robí sa to pomocou obslužného softvéru eID klient.", null);
     }
 }
