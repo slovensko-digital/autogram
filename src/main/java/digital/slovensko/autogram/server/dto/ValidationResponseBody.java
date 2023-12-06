ebf1d644d5a977e7f747a4df114e633336eecb57
Marek Ceľuch <celuchmarek@gmail.com>
2023-12-06 14:30:33 +0100

    translate the validaiton api response objects to english

--- src/main/java/digital/slovensko/autogram/server/dto/ValidationResponseBody.java
+++ src/main/java/digital/slovensko/autogram/server/dto/ValidationResponseBody.java
@@ -11,7 +11,7 @@ import eu.europa.esig.dss.validation.reports.Reports;
 import javax.security.auth.x500.X500Principal;
 import java.util.List;
 
-public record ValidationResponseBody(String formatSuboru, List<Podpis> podpisy, List<PodpisanyObjekt> podpisaneObjekty) {
+public record ValidationResponseBody(String fileFormat, List<Signature> signatures, List<SignedObject> signedObjects) {
 
     public static ValidationResponseBody build(Reports reports, DSSDocument document) throws DocumentNotSignedYetException {
         var sr = reports.getSimpleReport();
@@ -24,27 +24,27 @@ public record ValidationResponseBody(String formatSuboru, List<Podpis> podpisy, 
         var f = sr.getContainerType();
         var formatSuboru = f == null ? sr.getSignatureFormat(sr.getSignatureIdList().get(0)).getSignatureForm().name() : f.name();
 
-        List<Podpis> podpisy = dr.getSignatures().stream().map((e) -> {
+        List<Signature> podpisy = dr.getSignatures().stream().map((e) -> {
             var conclusion = e.getConclusion();
             var timestamps = e.getTimestamps();
             var signingCertificate = dd.getCertificateById(dd.getSigningCertificateId(e.getId()));
 
-            return new Podpis(
-                    new VysledokOvereniaPodpisu(
+            return new Signature(
+                    new Result(
                             conclusion.getIndication().ordinal(),
                             conclusion.getIndication().name()
                     ),
-                    new InformaciaOPodpise(
+                    new SignatureInfo(
                             sr.getSigningTime(e.getId()).toString(),
                             timestamps.isEmpty() ? null : sr.getProductionTime(timestamps.get(0).getId()).toString(),
                             sr.getSignatureFormat(e.getId()).name(),
-                            new Podpisovycertifikat(
+                            new CertificateInfo(
                                     new X500Principal(signingCertificate.getCertificateIssuerDN()).getName(X500Principal.RFC1779),
                                     new X500Principal(signingCertificate.getCertificateDN()).getName(X500Principal.RFC1779),
                                     signingCertificate.getSerialNumber(),
                                     signingCertificate.getNotBefore().toString(),
                                     signingCertificate.getNotAfter().toString(),
-                                    new Typ(
+                                    new Result(
                                             sr.getSignatureQualification(e.getId()).ordinal(),
                                             sr.getSignatureQualification(e.getId()).getReadable()
                                     )
@@ -52,13 +52,13 @@ public record ValidationResponseBody(String formatSuboru, List<Podpis> podpisy, 
                             !timestamps.isEmpty(),
                             timestamps.isEmpty() ? null : timestamps.stream().map((t) -> {
                                 var tsCertificate = dd.getCertificateById(dd.getTimestampSigningCertificateId(t.getId()));
-                                return new CertifikatCasovejPeciatky(
+                                return new CertificateInfo(
                                     new X500Principal(tsCertificate.getCertificateIssuerDN()).getName(X500Principal.RFC1779),
                                     new X500Principal(tsCertificate.getCertificateDN()).getName(X500Principal.RFC1779),
                                     tsCertificate.getSerialNumber(),
                                     tsCertificate.getNotBefore().toString(),
                                     tsCertificate.getNotAfter().toString(),
-                                    new Typ(
+                                    new Result(
                                             sr.getTimestampQualification(t.getId()).ordinal(),
                                             sr.getTimestampQualification(t.getId()).getReadable()
                                     )
@@ -68,11 +68,11 @@ public record ValidationResponseBody(String formatSuboru, List<Podpis> podpisy, 
                     ));
         }).toList();
 
-        List<PodpisanyObjekt> podpisaneObjekty = List.of();
+        List<SignedObject> podpisaneObjekty = List.of();
 
         switch (sr.getSignatureFormat(sr.getSignatureIdList().get(0)).getSignatureForm()) {
             case PAdES: {
-                podpisaneObjekty = dd.getAllSignerDocuments().stream().map((d) -> new PodpisanyObjekt(
+                podpisaneObjekty = dd.getAllSignerDocuments().stream().map((d) -> new SignedObject(
                         d.getId(),
                         MimeTypeEnum.PDF.getMimeTypeString(),
                         d.getReferencedName()
@@ -88,7 +88,7 @@ public record ValidationResponseBody(String formatSuboru, List<Podpis> podpisy, 
                     if (r.size() != 1)
                         return null;
                     var doc = r.get(0);
-                    return new PodpisanyObjekt(
+                    return new SignedObject(
                             d.getId(),
                             doc.getMimeType().getMimeTypeString(),
                             d.getReferencedName()
@@ -105,7 +105,7 @@ public record ValidationResponseBody(String formatSuboru, List<Podpis> podpisy, 
                     if (r.size() != 1)
                         return null;
                     var doc = r.get(0);
-                    return new PodpisanyObjekt(
+                    return new SignedObject(
                             d.getId(),
                             doc.getMimeType().getMimeTypeString(),
                             d.getReferencedName()
@@ -120,25 +120,19 @@ public record ValidationResponseBody(String formatSuboru, List<Podpis> podpisy, 
     }
 }
 
-record Podpis(VysledokOvereniaPodpisu vysledokOvereniaPodpisu, InformaciaOPodpise informaciaOPodpise) {
+record Signature(Result validaitonResult, SignatureInfo signatureInfo) {
 }
 
-record VysledokOvereniaPodpisu(int kod, String popis) {
+record Result(int code, String description) {
 }
 
-record InformaciaOPodpise(String datumACasPodpisuUtc, String datumACasCasovejPeciatkyPodpisuUtc, String typ,
-                          Podpisovycertifikat podpisovycertifikat, boolean obsahujeCasovuPeciatku,
-                          List<CertifikatCasovejPeciatky> certifikatyCasovejPeciatky, List<String> podpisaneObjekty) {
+record SignatureInfo(String claimedSigningTime, String timestampSigningTime, String level,
+                     CertificateInfo signingCertificate, boolean isTimestamped,
+                     List<CertificateInfo> timestamps, List<String> signedObjectsIds) {
 }
 
-record Podpisovycertifikat(String vydavatel, String subjekt, String serioveCislo, String platnostOd, String platnostDo, Typ typ) {
+record CertificateInfo(String issuerDN, String subjectDN, String serialNumber, String notBefore, String notAfter, Result qualification) {
 }
 
-record Typ(int kod, String popis) {
-}
-
-record CertifikatCasovejPeciatky(String vydavatel, String subjekt, String serioveCislo, String platnostOd, String platnostDo, Typ typ) {
-}
-
-record PodpisanyObjekt(String id, String mimeType, String nazov) {
+record SignedObject(String id, String mimeType, String filename) {
 }
