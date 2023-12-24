56e8684a7a039c2c61d913fdf6f66d6ea40e8720
Filip Seman <filip.seman@protonmail.com>
2023-12-24 14:11:52 +0100

    ci: add .deb .rpm builds in containers, update package script

--- src/main/scripts/package.sh
+++ src/main/scripts/package.sh
@@ -1,81 +1,90 @@
-#!/bin/bash -e
-jpackage=$1
-appDirectory=$2
-jdkDirectory=$3
-resourcesDir=$4
-platform=$5
-version=$6
-output=$7
+#!/bin/bash
+
+set -e
+
+jpackage=${1}
+appDirectory=${2}
+jdkDirectory=${3}
+resourcesDir=${4}
+platform=${5}
+version=${6}
+output=${7}
 
 function checkExitCode() {
-    exitValue=$1
-    if [[ $exitValue -ne 0 ]]; then
-        exit $exitValue
+    exitValue=${1}
+    if [[ ${exitValue} -ne 0 ]]; then
+        exit "${exitValue}"
     fi
 }
 
 IFS="="
 while read -r key value; do
     # Empty lines and comments
-    if [[ -z "$key" ]] || [[ -z "$value" ]] || [[ $key == \#* ]]; then
+    if [[ -z "${key}" ]] || [[ -z "${value}" ]] || [[ ${key} == \#* ]]; then
         continue
     fi
 
-    safekey=$(echo "$key" | tr . _)
-    trimmedvalue=$(echo "$value" | sed 's/[[:space:]]*$//g' | sed 's/^[[:space:]]*//g')
-    declare "properties_$safekey=$trimmedvalue"
+    safekey=$(echo "${key}" | tr . _)
+    trimmedvalue=$(echo "${value}" | sed 's/[[:space:]]*$//g' | sed 's/^[[:space:]]*//g')
+    declare "properties_${safekey}=${trimmedvalue}"
 done <"$resourcesDir/build.properties"
 unset IFS
 
-jvmOptions="-Dfile.encoding=UTF-8 -Dprism.maxvram=2G --add-exports javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED --add-exports jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED --add-opens java.base/java.security=ALL-UNNAMED --add-opens jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED"
+jvmOptions="-Dfile.encoding=UTF-8 \
+    -Dprism.maxvram=2G \
+    --add-exports javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED \
+    --add-exports jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED \
+    --add-opens java.base/java.security=ALL-UNNAMED \
+    --add-opens jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED"
+    
 arguments=(
-    "--input" "$appDirectory"
-    "--runtime-image" "$jdkDirectory"
+    "--input" "${appDirectory}"
+    "--runtime-image" "${jdkDirectory}"
     "--main-jar" "autogram.jar"
     "--app-version" "${properties_version:-$version}"
-    "--copyright" "$properties_copyright"
-    "--vendor" "$properties_vendor"
+    "--copyright" "${properties_copyright}"
+    "--vendor" "${properties_vendor}"
     "--icon" "./Autogram.png"
-    "--license-file" "$appDirectory/LICENSE"
+    "--license-file" "${appDirectory}/LICENSE"
     "--resource-dir" "./"
-    "--dest" "$output"
-    "--description" "$properties_description"
+    "--dest" "${output}"
+    "--description" "${properties_description}"
 )
 
-if [[ "$platform" == "win" ]]; then
+if [[ "${platform}" == "win" ]]; then
     cp "./main.template.wxs" "./main.wxs"
-    sed -i -e "s/PROTOCOL_NAME/$properties_protocol/g" "./main.wxs"
+    sed -i -e "s/PROTOCOL_NAME/${properties_protocol}/g" "./main.wxs"
 
     arguments+=(
-        "--name" "$properties_name"
+        "--name" "${properties_name}"
         "--type" "msi"
         "--icon" "./Autogram.ico"
-        "--java-options" "$jvmOptions --add-opens jdk.crypto.mscapi/sun.security.mscapi=ALL-UNNAMED"
+        "--java-options" "${jvmOptions} --add-opens jdk.crypto.mscapi/sun.security.mscapi=ALL-UNNAMED"
         "--win-shortcut-prompt"
         "--win-menu"
         "--add-launcher" "autogram-cli=$resourcesDir/windows-cli-build.properties"
     )
 
-    if [[ ! -z "$properties_win_upgradeUUID" ]]; then
+    if [[ ! -z "${properties_win_upgradeUUID}" ]]; then
         arguments+=(
-            "--win-upgrade-uuid" "$properties_win_upgradeUUID"
+            "--win-upgrade-uuid" "${properties_win_upgradeUUID}"
         )
     fi
 
-    if [[ -z "$properties_win_menu" ]] || [[ "$properties_win_menu" -ne "0" ]]; then
+    if [[ -z "${properties_win_menu}" ]] || [[ "${properties_win_menu}" -ne "0" ]]; then
         arguments+=(
             "--win-menu"
             "--win-menu-group" "${properties_win_menuGroup:-$properties_vendor}"
         )
     fi
 
-    if [[ "$properties_win_perUserInstall" == "1" ]]; then
+    if [[ "${properties_win_perUserInstall}" == "1" ]]; then
         arguments+=(
             "--win-per-user-install"
         )
     fi
 
-    if [[ "$properties_win_shortcut" == "1" ]]; then
+    if [[ "${properties_win_shortcut}" == "1" ]]; then
         arguments+=(
             "--win-shortcut"
         )
@@ -89,65 +98,67 @@ if [[ "$platform" == "linux" ]]; then
     cp "./autogram.template.desktop" "./autogram.desktop"
     sed -i -e "s/PROTOCOL_NAME/$properties_protocol/g" "./autogram.desktop"
 
-    if [[ ! -z "$properties_linux_debMaintainer" ]]; then
+    if [[ ! -z "${properties_linux_appCategory}" ]]; then
         arguments+=(
-            "--linux-deb-maintainer" "$properties_linux_debMaintainer"
+            "--linux-app-category" "${properties_linux_appCategory}"
         )
     fi
 
-    if [[ ! -z "$properties_linux_appCategory" ]]; then
+    if [[ ! -z "${properties_linux_packageDeps}" ]]; then
         arguments+=(
-            "--linux-app-category" "$properties_linux_appCategory"
-        )
-    fi
-
-    if [[ ! -z "$properties_linux_packageDeps" ]]; then
-        arguments+=(
-            "--linux-package-deps" "$properties_linux_packageDeps"
+            "--linux-package-deps" "${properties_linux_packageDeps}"
         )
     fi
 
     lowercase_name=$(echo "$properties_name" | tr '[:upper:]' '[:lower:]')
 
     arguments+=(
-        "--name" "$lowercase_name"
-        "--java-options" "$jvmOptions"
-        "--linux-rpm-license-type" "${properties_linux_rpmLicenseType:-MIT}"
+        "--name" "${lowercase_name}"
+        "--java-options" "${jvmOptions}"
         "--linux-menu-group" "${properties_linux_menuGroup:-Office}"
-        "--install-dir" "$properties_linux_installDir"
+        "--install-dir" "${properties_linux_installDir}"
     )
 
-    if [[ "$properties_linux_shortcut" == "1" ]]; then
+    if [[ "${properties_linux_shortcut}" == "1" ]]; then
         arguments+=(
             "--linux-shortcut"
         )
     fi
 
-    # Build both .rpm and .deb from Linux - should work from Debian-like distro but not the other way around
-    arguments+=(
-        "--type" "rpm"
-    )
-    $jpackage "${arguments[@]}"
-    checkExitCode $?
+    # Load variables of distribution
+    . /etc/os-release
 
-    if [[ -f "/etc/lsb-release" ]]; then
+    if [ "${ID}" == "fedora" ]; then
+        arguments+=(
+            "--type" "rpm"
+            "--linux-rpm-license-type" "${properties_linux_rpmLicenseType:-MIT}"
+        )
+    fi
+
+    if [ "${ID}" == "debian" ]; then
         arguments+=(
             "--type" "deb"
         )
-        $jpackage "${arguments[@]}"
-        checkExitCode $?
+        if [[ ! -z "${properties_linux_debMaintainer}" ]]; then
+            arguments+=(
+                "--linux-deb-maintainer" "${properties_linux_debMaintainer}"
+            )
+        fi
     fi
+
+    $jpackage "${arguments[@]}"
+    checkExitCode $?
 fi
 
-if [[ "$platform" == "mac" ]]; then
+if [[ "${platform}" == "mac" ]]; then
     cp "./Info.plist.template" "./Info.plist"
     sed -i.bak "s/PROTOCOL_NAME/$properties_protocol/g" "./Info.plist" && rm "./Info.plist.bak"
 
     arguments+=(
-        "--name" "$properties_name"
+        "--name" "${properties_name}"
         "--type" "pkg"
         "--icon" "./Autogram.icns"
-        "--java-options" "$jvmOptions"
+        "--java-options" "${jvmOptions}"
         "--mac-app-category" "${properties_mac_appCategory:-business}"
         "--mac-entitlements" "./Autogram.entitlements"
         # Building on mac requires modifying of image files
@@ -155,30 +166,30 @@ if [[ "$platform" == "mac" ]]; then
         "--temp" "./DTempFiles"
     )
 
-    if [[ ! -z "$properties_mac_identifier" ]]; then
+    if [[ ! -z "${properties_mac_identifier}" ]]; then
         arguments+=(
-            "--mac-package-identifier" "$properties_mac_identifier"
+            "--mac-package-identifier" "${properties_mac_identifier}"
         )
     fi
 
-    if [[ ! -z "$properties_mac_name" ]]; then
+    if [[ ! -z "${properties_mac_name}" ]]; then
         arguments+=(
-            "--mac-package-name" "$properties_mac_name"
+            "--mac-package-name" "${properties_mac_name}"
         )
     fi
 
     if [[ "$properties_mac_sign" == "1" ]]; then
         export JPACKAGE_MAC_SIGN="1"
-        if [[ -z "$APPLE_DEVELOPER_IDENTITY" ]] || [[ -z "$APPLE_KEYCHAIN_PATH" ]]; then
+        if [[ -z "${APPLE_DEVELOPER_IDENTITY}" ]] || [[ -z "${APPLE_KEYCHAIN_PATH}" ]]; then
             echo "Missing APPLE_DEVELOPER_IDENTITY or APPLE_KEYCHAIN_PATH env variable"
             exit 1
         fi
 
-        mac_signingKeyUserName=$(echo $APPLE_DEVELOPER_IDENTITY | sed -ne 's/Developer ID Application\:[[:space:]]\(.*\)[[:space:]]([0-9A-Z]*)/\1/p')
+        mac_signingKeyUserName=$(echo ${APPLE_DEVELOPER_IDENTITY} | sed -ne 's/Developer ID Application\:[[:space:]]\(.*\)[[:space:]]([0-9A-Z]*)/\1/p')
         arguments+=(
             "--mac-sign"
-            "--mac-signing-keychain" "$APPLE_KEYCHAIN_PATH"
-            "--mac-signing-key-user-name" "$mac_signingKeyUserName"
+            "--mac-signing-keychain" "${APPLE_KEYCHAIN_PATH}"
+            "--mac-signing-key-user-name" "${mac_signingKeyUserName}"
         )
     fi
 
