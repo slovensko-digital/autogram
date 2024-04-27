13ecc8f81d08c97cfaf7b8ec91672f4f5fd12461
matus <matesfila@gmail.com>
2024-04-27 19:19:52 +0200

    #367 - Pri výbere certifikátu vidno aj taký, ktorý na podpisovanie neslúži fix: certifikáty sa prefiltrujú iba na také, ktoré majú príznak DIGITAL_SIGNATURE

--- src/main/java/digital/slovensko/autogram/ui/gui/AboutDialogController.java
+++ src/main/java/digital/slovensko/autogram/ui/gui/AboutDialogController.java
@@ -4,6 +4,7 @@ import digital.slovensko.autogram.Main;
 import javafx.application.HostServices;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.AccessibleRole;
 import javafx.scene.Node;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.text.Text;
@@ -25,7 +26,21 @@ public class AboutDialogController implements SuppressedFocusController {
         versionText.setText(Main.getVersionString());
     }
 
+    public void initAccessible() {
+        mainBox.setAccessibleText("This is the main box of the About Dialog");
+        //mainBox.setAccessibleRole(AccessibleRole.CON);
+        mainBox.setAccessibleHelp("This box contains information about the project, authors, sponsors, and license");
+
+        link.setAccessibleText("This is a link to the GitHub page of the project");
+        //link.setAccessibleRole(AccessibleRole.LINK);
+        link.setAccessibleHelp("Click this link to open the GitHub page of the project in your web browser");
+        versionText.setAccessibleText("This is the version of the application");
+        versionText.setAccessibleRole(AccessibleRole.TEXT);
+        versionText.setAccessibleHelp("This text displays the current version of the application");
+    }
+
     public void githubLinkAction(ActionEvent ignored) {
+        //versionText.setAccessibleRole(AccessibleRole.);
         hostServices.showDocument("https://github.com/slovensko-digital/autogram");
     }
 
