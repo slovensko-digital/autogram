0051eb2d265b0e30d6838757267f97a8def0c2b4
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-07-12 21:42:21 +0200

    refactor(ServiceUnavailableException): remove dot at the end of sentence

--- src/main/java/digital/slovensko/autogram/core/errors/ServiceUnavailableException.java
+++ src/main/java/digital/slovensko/autogram/core/errors/ServiceUnavailableException.java
@@ -2,10 +2,10 @@ package digital.slovensko.autogram.core.errors;
 
 public class ServiceUnavailableException extends AutogramException {
     public ServiceUnavailableException(String url) {
-        super("Chyba pripojenia", "Spojenie so serverom zlyhalo", "Nepodarilo sa nadviazať spojenie so serverom na adrese: " + url + ".");
+        super("Chyba pripojenia", "Spojenie so serverom zlyhalo", "Nepodarilo sa nadviazať spojenie so serverom na adrese: " + url);
     }
 
     public ServiceUnavailableException(String url, Throwable e) {
-        super("Chyba pripojenia", "Spojenie so serverom zlyhalo", "Nepodarilo sa nadviazať spojenie so serverom na adrese: " + url + ".", e);
+        super("Chyba pripojenia", "Spojenie so serverom zlyhalo", "Nepodarilo sa nadviazať spojenie so serverom na adrese: " + url, e);
     }
 }
