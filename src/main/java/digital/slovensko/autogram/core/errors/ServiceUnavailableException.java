525f334c30d9b0fee5e21e45d23742cf9e66222f
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2024-07-14 21:32:24 +0200

    feat(ServiceUnavailableException): improve error message

--- src/main/java/digital/slovensko/autogram/core/errors/ServiceUnavailableException.java
+++ src/main/java/digital/slovensko/autogram/core/errors/ServiceUnavailableException.java
@@ -2,10 +2,10 @@ package digital.slovensko.autogram.core.errors;
 
 public class ServiceUnavailableException extends AutogramException {
     public ServiceUnavailableException(String url) {
-        super("Chyba pripojenia", "Spojenie so serverom zlyhalo", "Nepodarilo sa nadviazať spojenie so serverom na adrese: " + url);
+        this(url, null);
     }
 
     public ServiceUnavailableException(String url, Throwable e) {
-        super("Chyba pripojenia", "Spojenie so serverom zlyhalo", "Nepodarilo sa nadviazať spojenie so serverom na adrese: " + url, e);
+        super("Chyba pripojenia", "Spojenie so serverom zlyhalo", "Nepodarilo sa nadviazať spojenie so serverom na adrese: " + url + "\n\nSkúste podpisovanie opakovať neskôr. Ak problém pretrváva dlhodobo, ozvite sa nám, prosím, emailom na podpora@slovensko.digital", e);
     }
 }
