55a639cf7bc83490c6e5a26680e2989b39fb76af
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2025-01-18 09:22:23 +0100

    refactor: cleanup

--- src/test/java/digital/slovensko/autogram/XDCBuilderTests.java
+++ src/test/java/digital/slovensko/autogram/XDCBuilderTests.java
@@ -10,6 +10,7 @@ import eu.europa.esig.dss.enumerations.MimeTypeEnum;
 import eu.europa.esig.dss.enumerations.SignatureLevel;
 import eu.europa.esig.dss.enumerations.SignaturePackaging;
 import eu.europa.esig.dss.model.InMemoryDocument;
+
 import org.junit.jupiter.api.Test;
 
 import javax.xml.crypto.dsig.CanonicalizationMethod;
