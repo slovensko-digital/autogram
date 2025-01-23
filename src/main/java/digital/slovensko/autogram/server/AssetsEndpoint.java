1b042deb5ed9ebacc620613e8fe65520e4e7cf47
Marek Celuch <celuchmarek@gmail.com>
2025-01-23 02:20:42 -0800

    fix windows version

--- src/main/java/digital/slovensko/autogram/server/AssetsEndpoint.java
+++ src/main/java/digital/slovensko/autogram/server/AssetsEndpoint.java
@@ -6,23 +6,13 @@ import digital.slovensko.autogram.server.dto.ErrorResponse;
 import digital.slovensko.autogram.server.errors.InvalidUrlParamException;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
 public class AssetsEndpoint implements HttpHandler {
     private static final List<String> assets;
-    private static final Path assetsPath;
     static  {
-        try {
-            assetsPath = Paths.get(requireNonNull(AssetsEndpoint.class.getResource("index.html")).toURI()).getParent().resolve("assets");
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
         assets = List.of(
                 "swagger-ui-bundle-v5.11.0.js",
                 "swagger-ui-v5.11.0.css"
@@ -45,7 +35,7 @@ public class AssetsEndpoint implements HttpHandler {
                 throw new InvalidUrlParamException("Asset with this name does not exist");
 
             try (exchange) {
-                exchange.getResponseHeaders().set("Content-Type", Files.probeContentType(assetsPath.resolve(fileName)));
+                exchange.getResponseHeaders().set("Content-Type", getAssetMimeType(fileName));
                 exchange.sendResponseHeaders(200, 0);
                 requireNonNull(stream).transferTo(exchange.getResponseBody());
             }
@@ -54,5 +44,15 @@ public class AssetsEndpoint implements HttpHandler {
         } catch (IOException | InvalidUrlParamException e) {
             EndpointUtils.respondWithError(ErrorResponse.buildFromException(e), exchange);
         }
+    }
+
+    private String getAssetMimeType(String fileName) {
+        if (fileName.endsWith(".js"))
+            return "application/javascript";
+
+        if (fileName.endsWith(".css"))
+            return "text/css";
+
+        return "applicaiton/octet-stream";
     }
 }
