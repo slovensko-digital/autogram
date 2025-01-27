989823cccc49efd61d749eb5577754968bf41ecd
Marek Ceľuch <celuchmarek@gmail.com>
2025-01-27 09:50:47 +0100

    double quote every variable in mac os sh functions

--- src/main/scripts/resources/Autogram-post-image.sh
+++ src/main/scripts/resources/Autogram-post-image.sh
@@ -13,6 +13,6 @@ chmod +x "$TARGET/MacOS/Autogram"
 # codesign changed executables
 ENTITLEMENTS=../../Autogram.entitlements
 if [[ "$JPACKAGE_MAC_SIGN" == "1" ]]; then
-    codesign -s "$APPLE_DEVELOPER_IDENTITY" --keychain $APPLE_KEYCHAIN_PATH --entitlements "$ENTITLEMENTS" --options=runtime --deep --timestamp --force "$TARGET/MacOS/Autogram"
-    codesign -s "$APPLE_DEVELOPER_IDENTITY" --keychain $APPLE_KEYCHAIN_PATH --entitlements "$ENTITLEMENTS" --options=runtime --deep --timestamp --force "$TARGET/MacOS/AutogramApp"
+    codesign -s "$APPLE_DEVELOPER_IDENTITY" --keychain "$APPLE_KEYCHAIN_PATH" --entitlements "$ENTITLEMENTS" --options=runtime --deep --timestamp --force "$TARGET/MacOS/Autogram"
+    codesign -s "$APPLE_DEVELOPER_IDENTITY" --keychain "$APPLE_KEYCHAIN_PATH" --entitlements "$ENTITLEMENTS" --options=runtime --deep --timestamp --force "$TARGET/MacOS/AutogramApp"
 fi
