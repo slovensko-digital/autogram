0e7842f89c5e27e13ee80098c70ec020217680b3
Adrian Szegedi <adr.szegedi@gmail.com>
2025-04-30 11:16:38 +0200

    #541 add i18n resolving fallback

--- src/main/java/digital/slovensko/autogram/ui/gui/HasI18n.java
+++ src/main/java/digital/slovensko/autogram/ui/gui/HasI18n.java
@@ -1,12 +1,23 @@
 package digital.slovensko.autogram.ui.gui;
 
+import org.checkerframework.checker.propkey.qual.PropertyKey;
+
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public interface HasI18n {
     ResourceBundle getResources();
 
-    default String i18n(String key) {
-        return getResources().getString(key);
+    default String i18n(@PropertyKey String key) {
+        if (key == null) {
+            return "null";
+        }
+
+        try {
+            return getResources().getString(key);
+        } catch (MissingResourceException e) {
+            return key;
+        }
     }
 
     default String translate(String key) {
