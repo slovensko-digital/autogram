a194267e568ab5d50cab79aee1842dcb5ebb5308
Marek Ceľuch <celuchmarek@gmail.com>
2025-06-05 11:13:56 +0200

    add docker compose for linux builders and update packaging

---
+++ packaging/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /app
+export JAVA_HOME=/usr/lib/jvm/$(ls /usr/lib/jvm/ | grep bellsoft-java21-full)
+export PATH=$JAVA_HOME/bin:$PATH
+./mvnw versions:set -DnewVersion=${VERSION}
+./mvnw -B -C -V package -P system-jdk
+for ext in deb rpm; do
+    for i in $(ls target/autogram*.${ext} 2>/dev/null); do
+        mv "$i" "target/${PREFIX}_$(basename "$i")"
+    done
+    ls -lah ./target
+    cp target/${PREFIX}_*.${ext} /data/
+done
